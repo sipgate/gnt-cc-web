e8e8d230af7d5c7a5157f87d26e9d65389d5d80e
Julian Geywitz <geywitz@sipgate.de>
2020-02-07 14:59:07 +0100

    Initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="nav">
+    <b-navbar>
+      <template slot="brand">
+        <b-navbar-item tag="router-link" to='/'>
+          <img
+            class="brand-logo"
+            src="../assets/ganeti-logo.png"
+          >
+        </b-navbar-item>
+      </template>
+      <template slot="start">
+        <b-navbar-item tag="router-link" to="/">
+          Home
+        </b-navbar-item>
+        <b-navbar-item tag="router-link" to="/about">
+          About
+        </b-navbar-item>
+      </template>
+      <template slot="end">
+        <b-navbar-dropdown label="Cluster">
+          <b-navbar-item href="#">
+            HQ
+          </b-navbar-item>
+          <b-navbar-item href="#">
+            RZ1
+          </b-navbar-item>
+          <b-navbar-item href="#">
+            RZ2
+          </b-navbar-item>
+        </b-navbar-dropdown>
+      </template>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
+
+<style scoped>
+  .brand-logo {
+    max-height: 3rem;
+  }
+</style>
