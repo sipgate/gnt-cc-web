adee921d1a25f40112f7eed0ca8641e885ef9e68
Paul Schwörer <schwoerer@sipgate.de>
2020-04-03 12:24:14 +0200

    wip: add nice prettier config
    Co-authored-by: Robert Deppe <deppe@sipgate.de>

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,17 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/typescript/recommended',
-    'standard'
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint"
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    semi: ['error', 'always']
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   }
 };
